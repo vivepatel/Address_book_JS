566ad96ef04fd14c9b3396cd051a297aa9ae57d2
Vivekpatel <vp20032020@gmail.com>
2025-03-06 00:48:32 +0530

    [Vivek] Added UC2

--- Address_book_JS/index.js
+++ Address_book_JS/index.js
@@ -3,8 +3,21 @@ const Contact = require('./model/Contact');
 
 const addressBook = new AddressBook();
 
-const contact1 = new Contact(
-    "Vivek", "Patel", "Bhopal", "India", "M.P.", "101", "1234567890", "vp20032020@gmail.com"
-);
-console.log(addressBook.addContact(contact1));
-console.log(addressBook.getAllContacts());
+try {
+    const contact1 = new Contact(
+        "Muskan", "Gupta", "Bhopal", "India", "M.P.", "101", "9876543210", "muskan@gmail.com"
+    );
+    console.log(addressBook.addContact(contact1));
+    console.log(addressBook.getAllContacts());
+} catch (error) {
+    console.error("Error:", error.message);
+}
+
+try {
+    const invalidContact = new Contact(
+        "abc", "def", "ghi", "In", "mp", "1001", "12345", "muskan-email"
+    );
+    console.log(addressBook.addContact(invalidContact));
+} catch (error) {
+    console.error("Error:", error.message);
+}
--- Address_book_JS/model/Contact.js
+++ Address_book_JS/model/Contact.js
@@ -1,5 +1,14 @@
 class Contact {
     constructor(firstName, lastName, address, city, state, zip, phone, email) {
+        this.validateName(firstName, "First Name");
+        this.validateName(lastName, "Last Name");
+        this.validateAddress(address, "Address");
+        this.validateAddress(city, "City");
+        this.validateAddress(state, "State");
+        this.validateZip(zip);
+        this.validatePhone(phone);
+        this.validateEmail(email);
+
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
@@ -8,6 +17,40 @@ class Contact {
         this.zip = zip;
         this.phone = phone;
         this.email = email;
+    }
+
+    validateName(name, fieldName) {
+        const nameRegex = /^[A-Z][a-zA-Z]{2,}$/;
+        if (!nameRegex.test(name)) {
+            throw new Error(`${fieldName} must start with a capital letter and have at least 3 characters.`);
+        }
+    }
+
+    validateAddress(value, fieldName) {
+        if (value.length < 4) {
+            throw new Error(`${fieldName} must have at least 4 characters.`);
+        }
+    }
+
+    validateZip(zip) {
+        const zipRegex = /^[1-9][0-9]{5}$/;
+        if (!zipRegex.test(zip)) {
+            throw new Error("Invalid Zip Code. It must be a 6-digit number.");
+        }
+    }
+
+    validatePhone(phone) {
+        const phoneRegex = /^[6-9][0-9]{9}$/; 
+        if (!phoneRegex.test(phone)) {
+            throw new Error("Invalid Phone Number. It must be a 10-digit number starting with 6-9.");
+        }
+    }
+
+    validateEmail(email) {
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if (!emailRegex.test(email)) {
+            throw new Error("Invalid Email Address.");
+        }
     }
 }
 
