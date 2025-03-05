52a7e97dc6504d68bd6de792bf4c1f469a3ead94
Vivekpatel <vp20032020@gmail.com>
2025-03-06 01:47:42 +0530

    [Vivek] Added UC10

--- Address_book_JS/index.js
+++ Address_book_JS/index.js
@@ -6,24 +6,24 @@ const addressBook = new AddressBook();
 try {
    
     const contact1 = new Contact(
-    "Vivek", "Patel", "Bhopal", "India", "M.P.", "101001", "9876543210", "vivek-@gmail.com"
+    "Vivek", "Patel", "Bhopal", "India", "M.P.", "101009", "9876543210", "vivek@gmail.com"
     );
     console.log(addressBook.addContact(contact1)); 
 
     
 const contact2 = new Contact(
-    "Satyam", "Mishra", "Bhopal", "India", "M.P.", "100009", "9876541110", "satyam@gmail.com"
+    "Satyam", "Mishra", "Bhopal", "India", "M.P.", "100009", "9876541110", "stayam@gmail.com"
 );
 console.log(addressBook.addContact(contact2)); 
     console.log("All Contacts Before editing", addressBook.getAllContacts());
-    console.log(addressBook.editContact("Khushi", { address: "Mumbai", city: "New York", phone: "9238867890" }));
+    console.log(addressBook.editContact("Vivek", { address: "Mumbai", city: "New York", phone: "9238867890" }));
     console.log("All Contacts After editing", addressBook.getAllContacts());
-    console.log(addressBook.deleteContact("Khushi"));
+    console.log(addressBook.deleteContact("Vivek"));
     console.log("All Contacts After Deleting:", addressBook.getAllContacts());
     console.log("Total Contacts:", addressBook.countContacts()); 
 
     const duplicateContact = new Contact(
-        "Vivek", "patel", "Bhopal", "India", "M.P.", "101009", "9876543210", "vivek-@gmail.com"
+        "Vivek", "Patel", "Bhopal", "India", "M.P.", "101001", "9876543210", "vivek-@gmail.com"
     );console.log(addressBook.addContact(duplicateContact));  
 
     console.log("Contacts in India:");
@@ -31,6 +31,9 @@ console.log(addressBook.addContact(contact2));
 
     console.log("Viewing Persons by City or State:");
     console.log(addressBook.viewPersonsByCityOrState());
+
+    console.log("Count of Contacts by City and State:");
+    console.log(addressBook.countByCityOrState());
 }  catch (error) {
 console.error("Error:", error.message);
 }
